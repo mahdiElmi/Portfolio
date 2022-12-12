03500448c3d03eb9f35d78c7bdc19b10be661abb
mahdi <mahdi.azim.elmi@gmail.com>
2022-12-12 17:02:21 +0330

    everything ready exepct resume link

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      screens: {
+        semilg: "900px",
+      },
+      gridTemplateRows: {
+        layout: "75px auto auto 600px 600px 100px",
+      },
+    },
+  },
+  plugins: [],
+};
